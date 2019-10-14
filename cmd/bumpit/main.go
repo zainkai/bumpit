c316afd8c74cd7fab3813d3f9b7b563a20ad6e8b
Turkington, Kevin <kevin.turkington@nordstrom.com>
2019-10-14 16:10:53 -0700

    read and write file

--- cmd/bumpit/main.go
+++ cmd/bumpit/main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
 
-	"github.com/coreos/go-semver/semver"
 	"github.com/zainkai/bumpit/pkg/utils"
 )
 
+func isVersionNo(line string) bool {
+	versionStr := regexp.MustCompile(`(^|\s)\"version\": .*`)
+	return versionStr.MatchString(line)
+}
+
+func getVersion(line string) string {
+	v := regexp.MustCompile(`[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)?`)
+	return v.FindString(line)
+}
+
 func main() {
-	v := semver.New("1.2.3")
-	fmt.Println(v.String())
-	fmt.Println(utils.GetFilesInDir("."))
+	e, err := utils.NewEditor("./package.json")
+	if err != nil {
+		log.Fatalf("Could not create file editor", err)
+	}
+
+	e.EditLine(func(i int, line string) string {
+		if isVersionNo(line) {
+			return "FOUND IT\n"
+		}
+		return fmt.Sprintf("%d:  %s\n", i, line)
+	})
+
+	if err := e.CommitFile(); err != nil {
+		log.Fatalf("Could not commit changes to file", err)
+	}
 }
---
+++ pkg/utils/editor.go
@@ -0,0 +1,82 @@
+package utils
+
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+)
+
+type Editor struct {
+	Path string
+
+	FileIOReadFile  *os.File
+	FileIOWriteFile *os.File
+
+	ExistingFile *bufio.Scanner
+	TmpFile      *bufio.Writer
+}
+
+func NewEditor(path string) (*Editor, error) {
+	scannerHandle, err1 := os.Open(path)
+	if err1 != nil {
+		return nil, err1
+	}
+
+	writerHandle, err2 := ioutil.TempFile("", "bumpit.*.tmp")
+	if err2 != nil {
+		return nil, err2
+	}
+
+	existingFile := bufio.NewScanner(scannerHandle)
+	tmpFile := bufio.NewWriter(writerHandle)
+
+	editor := Editor{
+		Path:            path,
+		FileIOReadFile:  scannerHandle,
+		FileIOWriteFile: writerHandle,
+
+		ExistingFile: existingFile,
+		TmpFile:      tmpFile,
+	}
+
+	return &editor, nil
+}
+
+func (e *Editor) ReadLine(readLine func(int, string)) {
+	lineNum := 0
+	for e.ExistingFile.Scan() {
+		readLine(lineNum, e.ExistingFile.Text())
+		lineNum++
+	}
+}
+
+func (e *Editor) EditLine(editLine func(int, string) string) {
+	e.ReadLine(func(lineNo int, line string) {
+		newLine := editLine(lineNo, line)
+		e.TmpFile.WriteString(newLine)
+	})
+
+	e.TmpFile.Flush()
+}
+
+func (e *Editor) CommitFile() error {
+	TFile := e.FileIOReadFile.Name() + ".tmp"
+
+	if err := os.Rename(e.FileIOReadFile.Name(), TFile); err != nil {
+		return err
+	}
+	if err := os.Rename(e.FileIOWriteFile.Name(), e.Path); err != nil {
+		return err
+	}
+	if err := os.Remove(TFile); err != nil {
+		return err
+	}
+	if err := e.FileIOReadFile.Close(); err != nil {
+		return err
+	}
+	if err := e.FileIOWriteFile.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
